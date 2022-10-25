285ada20339350123626caefd3c96e6e42e7585b
ericminio <emignot@change.org>
2022-10-25 10:14:21 -0700

    use router more

--- app/about/1.model.spec.js
+++ app/about/1.model.spec.js
@@ -1,17 +1,30 @@
 import { expect } from 'chai';
-import { serving } from './support/serving.js';
+import { Server } from './support/server.js';
+import { Router } from './support/router.js';
 import { page } from './support/page.js';
 import { eventually } from './support/eventually.js';
-const server = serving('./app/index.html');
+import fs from 'fs';
 
+const router = new Router([
+    {
+        incoming: /GET \//,
+        handler: (request, response) => {
+            const html = fs.readFileSync('./app/index.html').toString();
+            response.setHeader('Content-Length', html.length);
+            response.setHeader('Content-Type', 'text/html');
+            response.end(html);
+        }
+    }
+]);
 describe('Displaying model', () => {
 
+    let server;
     beforeEach(done => {
+        server = new Server(5001, router.handler.bind(router));
         server.start(port => {
-            page.open(`http://localhost:${port}`).finally(done);
+            page.open(`http://localhost:${port}`).then(done).catch(done);
         });
-    });
-    afterEach(done => {
+    }); afterEach(done => {
         server.stop(done);
     });
 
--- app/start.js
+++ app/start.js
@@ -1,5 +1,31 @@
-import { serving } from './about/support/serving.js';
-const server = serving('./save.html');
+import { Router } from "./about/support/router.js";
+import { Server } from "./about/support/server.js";
+import fs from 'fs';
+import { payload } from './about/support/payload.js';
+
+const router = new Router([
+    {
+        incoming: /GET \//,
+        handler: (request, response) => {
+            const html = fs.readFileSync('./save.html').toString();
+            response.setHeader('Content-Length', html.length);
+            response.setHeader('Content-Type', 'text/html');
+            response.end(html);
+        }
+    },
+    {
+        incoming: /POST \/books/,
+        handler: async (request, response) => {
+            let incoming = await payload(request);
+            let answer = JSON.stringify({ data: `${request.method} ${request.url} ${incoming}` });
+            response.setHeader('Content-Length', answer.length);
+            response.setHeader('Content-Type', 'application/json');
+            response.end(answer);
+        }
+    }
+]);
+
+const server = new Server(5001, router.handler.bind(router));
 
 server.start(port => {
     console.log(`listening on port ${port}`);
